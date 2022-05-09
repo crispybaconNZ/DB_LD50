8363b2e78b248ffee861c17ed36b05a228767c94
crispybaconNZ <crispin.anderton@gmail.com>
2022-05-09 16:32:16 +1200

    Further reorganisation

--- Assets/Scripts/Managers/InstructionsManager.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class InstructionsManager : MonoBehaviour {
-    public void StartGame() {
-        SceneManager.LoadScene("Game");
-    }
-}
